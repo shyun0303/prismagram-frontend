c5c7e2c92865d359877416fe64b5063e811b4daf
shyun0303 <shyun0303@gmail.com>
2019-07-08 17:52:23 +0900

    start

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BODER = "1px solid #e6e6e6";
+const BORDER_RADIUS ="4px";
+export default {
+    bgColor: "#FAFAFA",
+    blackColor: "#262626",
+    blueColor:"#3897f0",
+    darkColor :"#003569",
+    lightGreyColor:"#c7c7c7",
+    redColor:"#ED4956",
+    darkGreyColor:"#999",
+    boxBorder: "1px solid #e6e6e6",
+    borderRadius:"4px",
+    whiteBox: `border:${BOX_BODER};
+               border-radius:${BORDER_RADIUS};
+               backgraound-color:white;
+               `
+
+}
